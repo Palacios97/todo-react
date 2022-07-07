acfcececa98edee3442eed0cee5546b8a27267b9
Miguel Palacios <Palacios.1997.miguel@gmail.com>
2022-07-07 17:34:21 +0200

    Updating job

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TodoList from './components/TodoList';
+
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <h1>TO DO LIST</h1>
+      <TodoList />
+      
+    </div>
+  )
+}
+
+export default App
+
